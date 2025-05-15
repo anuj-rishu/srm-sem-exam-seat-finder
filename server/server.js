c9345851895896697bc193513f5e6f79189ed23c
Anuj <anujtiwari4454@outlook.com>
2025-05-16 03:10:28 +0530

    add chnage

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const seatRoutes = require("./routes/seatRoutes");
-const { Analytics } = require("@vercel/analytics");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const seatRoutes = require('./routes/seatRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-if (process.env.NODE_ENV === "production") {
-  Analytics.inject();
-}
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api", seatRoutes);
+app.use('/api', seatRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ success: false, message: "Internal server error" });
+  res.status(500).json({ success: false, message: 'Internal server error' });
 });
 
 app.listen(PORT, () => {
