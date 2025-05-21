75628966328837f6c171951745273386671a5b42
Anuj <anujtiwari4454@outlook.com>
2025-05-21 20:03:47 +0530

    add msg

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -77,9 +77,13 @@ function App() {
            {showBanner && (
         <div className="bg-gradient-to-r from-blue-500 to-blue-600 text-white py-3 px-4 shadow-lg relative">
           <div className="container mx-auto text-center px-8">
-            <h2 className="text-xl md:text-2xl font-bold">
+            {/* <h2 className="text-xl md:text-2xl font-bold">
               🚀 SRM ACADEMIA BOT IS BACK ONLINE!
-            </h2>
+            </h2> */}
+                       <h6 className="text-sm md:text-lg font-bold">
+              ⚠️ 70k+ users in 4 days! We've hit Vercel's free tier limit—site may be down from 24 May.<br />
+              📌Join our Telegram bot for seat alerts.
+            </h6>
             <button 
               onClick={() => setShowBanner(false)}
               className="absolute top-2 right-4 md:top-3 md:right-6 bg-white/20 hover:bg-white/30 text-white p-1.5 rounded-full transition-all focus:outline-none"
