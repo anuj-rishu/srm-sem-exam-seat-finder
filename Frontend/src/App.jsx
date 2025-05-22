c4544692cabed65a2a4131635bae3b49b10f3aaa
Misbah Subhani <114570274+MisbahSubhani@users.noreply.github.com>
2025-05-22 18:51:18 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -80,10 +80,10 @@ function App() {
             {/* <h2 className="text-xl md:text-2xl font-bold">
               🚀 SRM ACADEMIA BOT IS BACK ONLINE!
             </h2> */}
-                       <h6 className="text-sm md:text-lg font-bold">
+{/*                        <h6 className="text-sm md:text-lg font-bold">
               ⚠️ 70k+ page view in 4 days! We've hit Vercel's free tier limit—site may be down from 24 May.<br />
               📌Join our Telegram bot for seat alerts.
-            </h6>
+            </h6> */}
             <button 
               onClick={() => setShowBanner(false)}
               className="absolute top-2 right-4 md:top-3 md:right-6 bg-white/20 hover:bg-white/30 text-white p-1.5 rounded-full transition-all focus:outline-none"
